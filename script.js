0e2756d9130e829ff84c5a0837d265517c52f8c5
TheFuckin <71916999+TheFuckin-com@users.noreply.github.com>
2025-08-18 08:48:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 const quotes = [
-    "One-of-a-kind.",
-    "Global shock appeal.",
-    "Off the market for 20 years.",
-    "Use it for whatever the fuck you want."
+  "\"One-of-a-kind\"",
+  "\"Global shock appeal\"",
+  "\"Off the market for 20 years\"",
+  "\"Use it for whatever the fuck you want\""
+  "\"Best email address ever\""
 ];
 
 let current = 0;
 const quoteEl = document.getElementById("quotes");
 
 setInterval(() => {
-    current = (current + 1) % quotes.length;
-    quoteEl.style.opacity = 0;
-    setTimeout(() => {
-        quoteEl.textContent = quotes[current];
-        quoteEl.style.opacity = 0.8;
-    }, 500);
+  current = (current + 1) % quotes.length;
+  quoteEl.style.opacity = 0;
+  setTimeout(() => {
+    quoteEl.textContent = quotes[current];
+    quoteEl.style.opacity = 0.8;
+  }, 500);
 }, 3000);
+
